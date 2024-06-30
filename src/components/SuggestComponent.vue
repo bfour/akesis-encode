ca555e7da43a93bda4c0fc4d41d46ccd5cac08f2
Florian Pollak <bfourdev@gmail.com>
2024-06-30 16:29:14 +0200

    add AWS sdk and vite fix

--- src/components/SuggestComponent.vue
+++ src/components/SuggestComponent.vue
@@ -43,20 +43,19 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import OpenAI from 'openai';
+
+import AWS from 'aws-sdk';
 import { useQuasar } from 'quasar';
 
 const quasar = useQuasar();
 
-const openAiConfig = {
-  organization: process.env.OPEN_AI_ORG_ID ?? '',
-  project: process.env.OPEN_AI_PROJECT_ID ?? '',
-  apiKey: process.env.OPEN_AI_API_KEY ?? '',
-  dangerouslyAllowBrowser: true,
-};
+var config = new AWS.Config({
+  accessKeyId: process.env.AWS_KEY ?? '',
+  secretAccessKey: process.env.AWS_SECRET ?? '',
+  region: process.env.AWS_REGION ?? '',
+});
+const comprehendMedical = new AWS.ComprehendMedical(config);
 
-const openAi = new OpenAI(openAiConfig);
 const text = ref<string>('');
 const suggestions = ref<string[]>([]);
 const isLoading = ref<boolean>(false);
@@ -65,24 +64,17 @@ const isMedicalSymptom = ref<boolean>(false);
 const onSubmit = async () => {
   try {
     isLoading.value = true;
-    const completion = await openAi.chat.completions.create({
-      messages: [
-        {
-          role: 'user',
-          content: `a patient is experiencing the following symptoms: ${text.value}. which additional information could a doctor be interested in? provide a concise list in JSON format, containing exactly two keys: 'suggestions' containing an array of strings that reflect the suggestions and 'isMedicalSymptom' containing a boolean value reflecting whether the given symptom description is describing medical symptoms (true) or is about another topic (false).`,
-        },
-      ],
-      response_format: { type: 'json_object' },
-      model: 'gpt-3.5-turbo',
-    });
-    console.log(completion);
+    const params = {
+      Text: 'The patient was prescribed 100mg of Ibuprofen to be taken twice daily.',
+    };
 
-    const suggestionsObj = JSON.parse(
-      completion.choices[0].message.content ?? ''
-    );
-    console.log(suggestionsObj);
-    suggestions.value = suggestionsObj.suggestions;
-    isMedicalSymptom.value = suggestionsObj.isMedicalSymptom;
+    comprehendMedical.detectEntities(params, (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(data);
+      }
+    });
   } catch (error) {
     console.error(error);
     quasar.notify({
