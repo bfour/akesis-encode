e0b237e12aa735237283164f08ad487821ea8c55
Florian Pollak <bfourdev@gmail.com>
2025-04-24 14:49:43 +0200

    Add yarn.lock file and update AWS credentials in index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ app.use(express.json());
 const comprehendMedical = new ComprehendMedicalClient({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    accessKeyId: process.env.AWS_KEY || '',
+    secretAccessKey: process.env.AWS_SECRET || '',
   },
 });
 
